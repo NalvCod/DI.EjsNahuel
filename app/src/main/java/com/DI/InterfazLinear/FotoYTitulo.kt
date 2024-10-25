402e68c52fe70540984096be511e36514f9c0f00
NalvCod <nahuelvhweb@gmail.com>
2024-10-25 17:33:57 +0200

    Final

---
+++ app/src/main/java/com/DI/InterfazLinear/FotoYTitulo.kt
@@ -0,0 +1,20 @@
+package com.DI.InterfazLinear
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FotoYTitulo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_foto_ytitulo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
